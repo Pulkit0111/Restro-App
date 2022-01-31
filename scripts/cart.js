f88ef16a6fc61d5ed0a7e75a7b061b6b6cd4401d
pulki <tjay@LAPTOP-EQMPOD1H.localdomain>
2022-01-31 19:55:46 +0530

    added removing functionality in cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,22 +2,31 @@ var cart_items=JSON.parse(localStorage.getItem("cart"));
   console.log(cart_items);
   var total=document.getElementById("total");
   var cart_page=document.getElementById("cart");
-  cart_items.forEach(element => {
-    var parent=document.createElement("div");
-    var img=document.createElement("img");
-    var p=document.createElement("p");
-    var price=document.createElement("p");
-    var btn=document.createElement("button");
-    img.src=element.image;
-    p.textContent=element.meal;
-    price.textContent=`Rs. ${element.price}`;
-    btn.textContent="Remove";
-    // btn.addEventListener("click",()=>{
-    // })
-    parent.append(img,p,price,btn)
-    parent.setAttribute("class","parent");
-    cart_page.append(parent);
-  });
+  const cart_data=(x)=>{
+    x=JSON.parse(localStorage.getItem("cart"));
+    x.forEach(element => {
+      var parent=document.createElement("div");
+      var img=document.createElement("img");
+      var p=document.createElement("p");
+      var price=document.createElement("p");
+      var btn=document.createElement("button");
+      img.src=element.image;
+      p.textContent=element.meal;
+      price.textContent=`Rs. ${element.price}`;
+      btn.textContent="Remove";
+      btn.addEventListener("click",()=>{
+        cart_page.innerHTML="";
+        var arr=x.filter(i=>i.meal!==element.meal);
+        window.localStorage.clear();
+        localStorage.setItem("cart",JSON.stringify(arr));
+        cart_data(arr);
+      })
+      parent.append(img,p,price,btn)
+      parent.setAttribute("class","parent");
+      cart_page.append(parent);
+    });
+  }
+  cart_data(cart_items);
   var h2=document.createElement("h2");
   var sum=0;
   cart_items.forEach(ele=>{
